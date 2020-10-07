9e9c9f0713a5d79c154a617566c8ddcb5be770ea
Mario H. Adaniya <mhadaniya@gmail.com>
2020-10-06 22:20:50 -0300

    :pencil: busca binaria recursiva, interativa e linear

--- 09-14/src/main/java/src/App.java
+++ 09-14/src/main/java/src/App.java
@@ -3,6 +3,22 @@ package src;
 public class App {
 
     public static void main(String[] args) {
+        int[] valoresTeste = { 10, 100, 1000, 10000, 100000, 1000000, 10000000 };
+
+        for (int i = 0; i < valoresTeste.length; i++) {
+            System.out.println("vetor[" + valoresTeste[i] + "]");
+            Vetor vetor = new Vetor();
+            for (int j = 0; j < valoresTeste[i]; j++) {
+                vetor.adicionar(j);
+            }
+            long startTime = System.currentTimeMillis();
+            vetor.contains(valoresTeste[i] - 1);
+            long stoppedTime = System.currentTimeMillis();
+            System.out.println((stoppedTime - startTime));
+        }
+
+        // vetor[10,100,1000,10000,100000,1000000]
+
         // int vetor[] = { 1, 2, 3, 4, 5 };
         // int vetor2[] = new int[6];
         // for (int i = 0; i < vetor2.length; i++) {
@@ -12,18 +28,23 @@ public class App {
         // vetor[3] = 3;
         // int vetor2[] = vetor;
         // vetor2[2] = 2;
-        Vetor vetor = new Vetor();
-        System.out.println(vetor);
-        vetor.adicionar(10);
-        vetor.adicionar(11);
-        vetor.adicionar(12);
-        vetor.adicionar(13);
-        vetor.adicionar(14);
-        System.out.println(vetor);
-        vetor.adicionar(20);
-        System.out.println(vetor);
-        System.out.println(vetor.contains(13));
-        System.out.println(vetor.contains(43));
+
+        // Vetor vetor = new Vetor();
+        // System.out.println(vetor);
+        // vetor.adicionar(10);
+        // vetor.adicionar(11);
+        // vetor.adicionar(12);
+        // vetor.adicionar(13);
+        // vetor.adicionar(14);
+        // System.out.println(vetor);
+        // vetor.adicionar(20);
+        // vetor.adicionar(21);
+        // vetor.adicionar(22);
+        // vetor.adicionar(30);
+        // vetor.adicionar(36);
+        // System.out.println(vetor);
+        // System.out.println(vetor.contains(13));
+        // System.out.println(vetor.contains(43));
 
     }
 }
--- 09-14/src/main/java/src/Vetor.java
+++ 09-14/src/main/java/src/Vetor.java
@@ -43,7 +43,7 @@ public class Vetor {
      */
     public void adicionar(int valor) {
         if (lastIndex == elements.length) {
-            int newTotalElements = elements.length * 2;
+            int newTotalElements = elements.length + 5;
             int newElements[] = new int[newTotalElements];
             for (int i = 0; i < elements.length; i++) {
                 newElements[i] = elements[i];
@@ -65,28 +65,53 @@ public class Vetor {
     }
 
     public Boolean contains(int element) {
-        int size = elements.length;
-        int min = 0;
-        int max = size - 1;
-        while (min <= max) {
-            int mid = (min + max) / 2;
-            int current = elements[mid];
-            if (element == current) {
-                return true;
-            } else if (current < element) {
-                min = mid + 1;
-            } else {
-                max = mid - 1;
-            }
-        }
-        return false;
+        // busca aleatoria
 
-        // for (int i = 0; i < elements.length; i++) {
-        // if (elements[i] == element)
+        // busca binaria recursiva
+        // int size = elements.length;
+        // int min = 0;
+        // int max = size - 1;
+
+        // return search(elements, element, min, max);
+
+        // Busca binaria iterativa
+        // int size = elements.length;
+        // int min = 0;
+        // int max = size - 1;
+        // while (min <= max) {
+        // int mid = (min + max) / 2;
+        // int current = elements[mid];
+        // if (element == current) {
         // return true;
+        // } else if (current < element) {
+        // min = mid + 1;
+        // } else {
+        // max = mid - 1;
+        // }
         // }
         // return false;
 
+        // busca linear
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == element)
+                return true;
+        }
+        return false;
+    }
+
+    public Boolean search(int[] elements, int element, int min, int max) {
+        // busca binaria recursiva
+        int mid = (min + max) / 2;
+        int current = elements[mid];
+        if (element == current) {
+            return true;
+        } else if (min > max) {
+            return false;
+        } else if (current < element) {
+            return search(elements, element, mid + 1, max);
+        } else {
+            return search(elements, element, min, mid - 1);
+        }
     }
 
     @Override
